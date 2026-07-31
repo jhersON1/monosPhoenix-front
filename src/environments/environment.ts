fb3eb06a94e8fe99b77270e131455c8745866bcb
Mauricio Aleman <mauricio.aleman.3524@gmail.com>
2026-07-31 15:28:58 -0400

    environment url change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'https://aod2avobq32n46m2ao6ftbtxzu0szcyw.lambda-url.us-east-2.on.aws'
+  apiUrl: 'https://statements-validation-worlds-ave.trycloudflare.com'
 };
