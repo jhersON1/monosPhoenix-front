c481cd9c3eb91833bddf04a1244298bcac4ac4fe
MarioQuimetMed <mario.medina@ficct.uagrm.edu.bo>
2026-08-13 23:14:47 -0400

    feature/update apiUrl in enviroment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'https://statements-validation-worlds-ave.trycloudflare.com'
+  apiUrl: 'http://a3466d33cfe644239b6c9d7ae1a34b58-610291338.us-east-1.elb.amazonaws.com'
 };
